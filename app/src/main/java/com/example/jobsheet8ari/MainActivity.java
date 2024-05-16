2f339a420bec6122adc475fe5a56d3a3371da593
laniadty <laniaditya65@gmail.com>
2024-05-16 15:36:35 +0700

    Final Commit

---
+++ app/src/main/java/com/example/jobsheet8ari/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.jobsheet8ari;
+
+import static com.example.jobsheet8ari.R.id.main;
+import static com.example.jobsheet8ari.R.id.pindah;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+
+import com.example.jobsheet8ari.MainActivity2;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(pindah), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void pindah(View view) {
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(intent);
+    }
+}
